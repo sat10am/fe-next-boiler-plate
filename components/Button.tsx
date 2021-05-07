a1bcceb8ea7a44921617bacbdd2a3a181d40ffdf
amorfati0310 <lyhy0310@gmail.com>
2021-05-08 02:45:54 +0900

    feat: emotion( style 도구) 설정

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,3 +9,4 @@ export default function Button({onClick}: ButtonProps) {
     </>
   )
 };
+
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,22 @@
 import Button from '@/components/Button'
+import styled from '@emotion/styled'
 
-export default function About() {
+export default function AboutPage() {
   return (
     <div>
       <main>
-        about Page
+        <Title>About Page</Title>
         <Button onClick={() => {console.log('clicked')}}/>
       </main>
     </div>
   )
-}
+};
+
+
+const Title = styled.div`
+  color: tomato;
+  font-size: 42px;
+`;
+
+
+
